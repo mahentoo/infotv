3f114600de638e87efdda194496926f65e9a33fe
Marinho <marinho.bcsc@gmail.com>
2020-07-21 22:56:45 -0300

    Create Signin.vue

---
+++ src/views/Signin.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="register">
+    <form @submit.prevent="signin()">
+      <div>
+        <label>E-mail</label>
+        <input type="email" name="email" v-model="email">
+      </div>
+      <div>
+        <label>Senha</label>
+        <input type="password" name="password" v-model="password">
+      </div>
+      <div v-if="error">{{ error }}</div>
+      <div>
+        <button type="submit">Entrar</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+  const axios = require('axios');
+
+  export default {
+    name: 'Signin',
+
+    data: () => ({
+      name: '',
+      email: '',
+      password: '',
+      password_confirmation: '',
+      error: '',
+    }),
+
+    methods: {
+      signin () {
+        this.error = '';
+
+        axios.post('http://front-test.diga.net.br/api/signin', {
+          email: this.email,
+          password: this.password,
+        })
+        .then(({ data }) => {
+          if (data.error) {
+            this.error = data.response.message;
+
+            return;
+          }
+
+          this.$router.replace('/');
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+      },
+    },
+  };
+</script>
