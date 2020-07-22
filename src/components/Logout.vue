8077c3339ba4a922dc2027956437ce886cabd8ce
Marinho <marinho.bcsc@gmail.com>
2020-07-22 00:03:10 -0300

    Create Logout.vue

---
+++ src/components/Logout.vue
@@ -0,0 +1,17 @@
+<template>
+  <button type="button" @click.prevent="clear()">Sair</button>
+</template>
+
+<script>
+  export default {
+    name: 'Signin',
+
+    methods: {
+      clear () {
+        window.localStorage.clear();
+
+        this.$router.replace('/login');
+      },
+    },
+  };
+</script>
