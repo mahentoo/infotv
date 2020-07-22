533ffd3bae8b2fa17f74c0f67f3c3f93c0b5e0b5
Marinho <marinho.bcsc@gmail.com>
2020-07-21 21:44:49 -0300

    Create Signup.vue

---
+++ src/views/Signup.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="register">
+    <form>
+      <div>
+        <label>E-mail</label>
+        <input type="email" name="email" v-model="email" required>
+      </div>
+      <div>
+        <label>Senha</label>
+        <input type="password" name="password" v-model="password" required>
+      </div>
+      <div>
+        <label>Confirmar senha</label>
+        <input type="password" name="password_confirmation" v-model="password_confirmation" required>
+      </div>
+      <div>
+        <button type="submit">Registrar</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Signup',
+
+    data: () => ({
+      email: '',
+      password: '',
+      'password_confirmation': '',
+    }),
+  };
+</script>
