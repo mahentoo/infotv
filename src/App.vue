f23cc57fe0082840264125d2a954f5ae11dd7f32
Marinho <marinho.bcsc@gmail.com>
2020-07-22 22:34:03 -0300

    Portuguese

--- src/App.vue
+++ src/App.vue
@@ -13,21 +13,21 @@
           <v-list-item-icon>
             <v-icon>mdi-face</v-icon>
           </v-list-item-icon>
-          <v-list-item-title>Sign Up</v-list-item-title>
+          <v-list-item-title>Cadastrar</v-list-item-title>
         </v-list-item>
 
         <v-list-item to="/signin" link>
           <v-list-item-icon>
             <v-icon>mdi-lock-open</v-icon>
           </v-list-item-icon>
-          <v-list-item-title>Sign In</v-list-item-title>
+          <v-list-item-title>Entrar</v-list-item-title>
         </v-list-item>
 
         <v-list-item @click="logout()" link>
           <v-list-item-icon>
             <v-icon>mdi-power</v-icon>
           </v-list-item-icon>
-          <v-list-item-title>Sign Out</v-list-item-title>
+          <v-list-item-title>Sair</v-list-item-title>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
