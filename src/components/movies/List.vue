a6b6b9b6db69458699bd03d37463fe04d84f4cf5
Marinho <marinho.bcsc@gmail.com>
2020-07-23 12:48:15 -0300

    Create List.vue

---
+++ src/components/movies/List.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container fluid></v-container>
+</template>
+
+<script>
+  export default {
+    name: 'List',
+
+    props: {
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  };
+</script>
