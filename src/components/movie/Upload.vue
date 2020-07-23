1544ad23f15fc047930336c203135f1d8ec36b84
Marinho <marinho.bcsc@gmail.com>
2020-07-23 15:09:11 -0300

    Input movie.name

--- src/components/movie/Upload.vue
+++ src/components/movie/Upload.vue
@@ -1,6 +1,20 @@
 <template>
   <v-form id="form-movie-upload" @submit.prevent="upload()">
     <v-layout column>
+      <v-flex>
+        <v-text-field
+          id="name"
+          name="name"
+          label="Nome"
+          type="text"
+          v-model="movie.name"
+          required
+        ></v-text-field>
+      </v-flex>
+      <v-flex class="text-xs-center" v-if="errors && errors.name">
+        <span class="red--text">{{ errors.name[0] }}</span>
+      </v-flex>
+
       <v-flex class="text-xs-center" mt-5>
         <v-btn type="submit" form="form-movie-upload" color="primary">
           Enviar
@@ -24,7 +38,9 @@
     },
 
     data: () => ({
-      movie: {},
+      movie: {
+        name: '',
+      },
       errors: null,
     }),
 
