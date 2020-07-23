6fa052ef361626d94393c9b7448f075facc20be4
Marinho <marinho.bcsc@gmail.com>
2020-07-23 20:55:06 -0300

    Action remove

--- src/components/movie/Card.vue
+++ src/components/movie/Card.vue
@@ -5,10 +5,18 @@
     <v-container>
       <v-img height="160" width="200" :src="movie.thumb_path"></v-img>
     </v-container>
+
+    <v-card-actions>
+      <v-btn @click="remove()" icon>
+        <v-icon>mdi-delete</v-icon>
+      </v-btn>
+    </v-card-actions>
   </v-card>
 </template>
 
 <script>
+  const axios = require('axios');
+
   export default {
     name: 'Card',
 
@@ -16,6 +24,30 @@
       movie: {
         type: Object,
         required: true,
+      },
+    },
+
+    methods: {
+      remove () {
+        const token = localStorage.getItem('token');
+
+        axios
+          .delete('http://front-test.diga.net.br/api/movie/5' + this.movie.id, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+              'Content-Type': 'application/x-www-form-urlencoded',
+            },
+          })
+          .then(({ data }) => {
+            if (data.error) {
+              this.error = data.response.message;
+
+              return;
+            }
+          })
+          .catch((error) => {
+            console.log(error);
+          });
       },
     },
   };
