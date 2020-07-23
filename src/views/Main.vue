ca199d4576d0fc05dd5994eba57ecd4c82bec040
Marinho <marinho.bcsc@gmail.com>
2020-07-23 01:40:24 -0300

    Create Main.vue

---
+++ src/views/Main.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container fluid></v-container>
+</template>
+
+<script>
+  import { Bus } from '@/main';
+
+  export default {
+    name: 'Main',
+
+    created () {
+      Bus.$on('currentUserUpdated', () => this.token = localStorage.getItem('user.token'));
+    },
+
+    data: () => ({
+      token: localStorage.getItem('user.token'),
+    }),
+  }
+</script>
