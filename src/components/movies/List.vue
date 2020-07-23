564a84db06b3614c8aa09d243b9c2cb1c20281fd
Marinho <marinho.bcsc@gmail.com>
2020-07-23 13:45:28 -0300

    Get movies from api

--- src/components/movies/List.vue
+++ src/components/movies/List.vue
@@ -3,6 +3,8 @@
 </template>
 
 <script>
+  const axios = require('axios');
+
   export default {
     name: 'List',
 
@@ -10,6 +12,26 @@
       token: {
         type: String,
         required: true,
+      },
+    },
+
+    created () {
+      this.list();
+    },
+
+    methods: {
+      list () {
+        axios
+          .get('http://front-test.diga.net.br/api/movie', {
+            headers: {
+              Authorization: `Bearer ${this.token}`,
+            },
+          })
+          .then(({ data }) => {
+          })
+          .catch((error) => {
+            console.log(error);
+          });
       },
     },
   };
