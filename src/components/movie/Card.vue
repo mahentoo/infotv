43af011af242a8c57da93b737ce6a61d2e8b8ec2
Marinho <marinho.bcsc@gmail.com>
2020-07-23 20:36:05 -0300

    Show thumbnail

--- src/components/movie/Card.vue
+++ src/components/movie/Card.vue
@@ -1,6 +1,10 @@
 <template>
   <v-card class="d-inline-block mx-auto">
     <v-card-title>{{ movie.name }}</v-card-title>
+
+    <v-container>
+      <v-img height="160" width="200" :src="movie.thumb_path"></v-img>
+    </v-container>
   </v-card>
 </template>
 
