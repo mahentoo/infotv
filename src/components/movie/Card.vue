d1a41d3b36152ac3308ec9ff1ef57001d04bf543
Marinho <marinho.bcsc@gmail.com>
2020-07-23 20:15:49 -0300

    Create Card.vue

---
+++ src/components/movie/Card.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-card class="d-inline-block mx-auto">
+    <v-card-title>{{ movie.name }}</v-card-title>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    name: 'Card',
+
+    props: {
+      movie: {
+        type: Object,
+        required: true,
+      },
+    },
+  };
+</script>
