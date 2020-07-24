d9c111615bddb7841f9a535edfaa7dac77b3ffc3
Marinho <marinho.bcsc@gmail.com>
2020-07-24 01:29:42 -0300

    More width

--- src/components/movie/Card.vue
+++ src/components/movie/Card.vue
@@ -1,9 +1,9 @@
 <template>
-  <v-card class="d-inline-block mr-4 mb-4">
+  <v-card class="d-inline-block mr-4 mb-4" width="284px">
     <v-card-title>{{ movie.name }}</v-card-title>
 
     <v-container>
-      <v-img height="160" width="200" :src="movie.thumb_path"></v-img>
+      <v-img height="160" width="260" :src="movie.thumb_path"></v-img>
     </v-container>
 
     <v-card-actions>
