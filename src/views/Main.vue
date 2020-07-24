49073ef1027be95e1ce5f9c66d139f5495eabfef
Marinho <marinho.bcsc@gmail.com>
2020-07-24 00:56:46 -0300

    Component <movie-upload> is enough

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,15 +1,7 @@
 <template>
   <v-layout row wrap v-if="token">
-    <v-flex class="text-right" xs12 mt-3 mx-5>
-      <v-btn @click="showUpload = ! showUpload" dark rounded v-if="showUpload">
-        Cancelar
-      </v-btn>
-      <v-btn color="primary" @click="showUpload = ! showUpload" dark rounded v-else>
-        Enviar vídeo
-      </v-btn>
-    </v-flex>
-    <v-flex xs12 sm6 xl4 offset-sm3 offset-xl4 mt-3>
-      <movie-upload @added="showUpload = false" v-show="showUpload"></movie-upload>
+    <v-flex xs12 mt-3 mx-5>
+      <movie-upload></movie-upload>
     </v-flex>
 
     <v-flex xs12 mt-3 mx-5>
