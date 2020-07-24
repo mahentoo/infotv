c34f4c35856a7a20de259d294b742d052cad68c9
Marinho <marinho.bcsc@gmail.com>
2020-07-24 05:09:43 -0300

    Load from store

--- src/views/Watch.vue
+++ src/views/Watch.vue
@@ -8,18 +8,7 @@
 
     computed: {
       movie () {
-        return {
-          created:'2020-07-23T22:47:17.000000Z',
-          file_path:'http://front-test.diga.net.br/storage/movies/h8vmw2RnujSdHoPNtcqBoCJD54AGEiRsgTrlGCf7mYaxGTBLAO4l.mp4',
-          file_size: {
-            human:'445.48kB',
-            raw:'456174',
-          },
-          id:6,
-          length:'27',
-          name:'Small',
-          thumb_path:'http://front-test.diga.net.br/storage/thumb/h8vmw2RnujSdHoPNtcqBoCJD54AGEiRsgTrlGCf7mYaxGTBLAO4l.jpg',
-        };
+        return this.$store.state.movies.find((movie) => movie.id == this.$route.params.id);
       }
     },
   };
