c36df84c23c067892130c6aee804e052fa81fbbf
Marinho <marinho.bcsc@gmail.com>
2020-07-24 03:21:42 -0300

    Vue add vuex

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import vuetify from './plugins/vuetify';
+import store from './store'
 
 Vue.component('MovieCard', require('./components/movie/Card.vue').default);
 Vue.component('MovieList', require('./components/movie/List.vue').default);
@@ -15,5 +16,6 @@ export const Bus = new Vue();
 new Vue({
   router,
   vuetify,
+  store,
   render: h => h(App)
 }).$mount('#app');
