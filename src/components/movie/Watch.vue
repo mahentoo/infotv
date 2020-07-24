e8b6410fe9e4c537123cc5554ac37cf8e794bb7d
Marinho <marinho.bcsc@gmail.com>
2020-07-24 03:17:21 -0300

    Create Watch.vue

---
+++ src/components/movie/Watch.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-card>
+    <video-player class="video-player-box" ref="videoPlayer" :options="playerOptions"></video-player>
+  </v-card>
+</template>
+
+<script>
+  import 'video.js/dist/video-js.css';
+
+  import { videoPlayer } from 'vue-video-player';
+
+  export default {
+    name: 'Watch',
+
+    components: {
+      videoPlayer,
+    },
+
+    props: {
+      movie: {
+        type: Object,
+        required: true,
+      },
+    },
+
+    computed: {
+      playerOptions () {
+        return {
+          language: 'en',
+          playbackRates: [0.7, 1.0, 1.5, 2.0],
+          sources: [{
+            type: 'video/mp4',
+            src: this.movie.file_path,
+          }],
+          poster: this.movie.thumb_path,
+        };
+      },
+    },
+  };
+</script>
