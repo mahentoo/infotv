bc3178bd23f224e40283cdf6c2497b089c34d3ca
Marinho <marinho.bcsc@gmail.com>
2020-08-07 19:44:45 -0300

    Using editing to help replace

--- src/components/movie/Upload.vue
+++ src/components/movie/Upload.vue
@@ -144,7 +144,7 @@ export default {
 
           this.$store.commit(mutation, {
             movie,
-            oldMovie: this.movie,
+            oldMovie: this.editing,
           });
 
           this.toggleDialog();
@@ -223,7 +223,8 @@ export default {
     },
 
     toggleDialog(movie = { name: '' }) {
-      this.movie = movie;
+      this.editing = movie;
+      this.movie = { ...movie };
       this.showDialog = !this.showDialog;
     },
   },
